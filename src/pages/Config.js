ae7faf135c6338120f2baa6bff8ec1b7e54e3262
Victor Gazolli <victorgazolli@compmac.local>
2019-10-27 22:59:35 -0300

    config screen modify

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -1,95 +1,114 @@
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, KeyboardAvoidingView, ScrollView, Platform, TextInput, StyleSheet, Image, View, Text, TouchableOpacity, ToastAndroid } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
-
-
-var config = {
-    host: '192.168.1.1', //ip address of the mssql database
-    user: 'sa', //username to login to the database
-    pass: 'password', //password to login to the database
-    db: 'admin', //the name of the database to connect to
-    port: 1433 //OPTIONAL, port of the database on the server
+import api from '../services/api';
+var account = {
+    cd_usuario: '',
+    nm_usuario: '',
+    cd_senha: '',
+    nm_email: '',
 }
+var newPass = ''
 
 export default function Main({ navigation }) {
+    const [isNew, updateisNew] = useState(false);
+    useEffect(async () => {
+        try {
+            AsyncStorage.getItem('@account_id').then(stored_id => {
+                try {
+                    if (stored_id.length > 0) {
+                        async function getAccount() {
+                            const { data } = await api.get('/user/' + stored_id)
+                            account = {
+                                name: data.nm_usuario,
+                                email: data.nm_email,
+                                password: data.cd_senha,
+                                id: data.cd_usuario,
+                            }
+                            updateisNew(!isNew)
+                        }
+                        getAccount()
+                    }
+                } catch{
 
-    async function handleConfig() {
-        if (config.host.length > 0 && config.port.length > 0 && config.user.length > 0 && config.pass.length > 0 && config.db.length > 0) {
+                }
+
+            })
+
+        } catch (err) {
+
+        }
+
+    }, []);
+    async function handleSubmit() {
+        if (account.cd_usuario.length > 0 && account.nm_usuario.length > 0 && account.cd_senha.length > 0 && account.nm_email.length > 0) {
             try {
-                navigation.navigate("Principal", config)
                 ToastAndroid.show("Salvo!", ToastAndroid.SHORT);
+                navigation.navigate("Produtos", account)
             } catch (error) {
-                ToastAndroid.show("Problema ao criar conta", ToastAndroid.SHORT);
+                ToastAndroid.show("Problema ao salvar", ToastAndroid.SHORT);
                 console.log(error)
             }
 
         } else {
-            ToastAndroid.show("Digite o host", ToastAndroid.SHORT);
+            ToastAndroid.show("Digite os campos", ToastAndroid.SHORT);
         }
 
     }
     return (
-        <ScrollView style={{ flex: 1 }}>
-            <KeyboardAvoidingView
-                behavior={Platform.OS === "ios" ? "padding" : null}
-            >
-                {/* <SafeAreaView> */}
-                    <View style={styles.body}>
-                        <View style={styles.form}>
-                            <Text style={styles.label}>Digite o host</Text>
-                            <TextInput
-                                onChangeText={val => config.host = val}
-                                placeholder="HOST"
-                                autoCapitalize="none"
-                                autoCorrect={false}
-                                style={styles.input}
-                                maxLength={40}
-                            />
-                            <Text style={styles.label}>Digite a Porta</Text>
-                            <TextInput
-                                onChangeText={val => config.port = val}
-                                placeholder="PORTA"
-                                autoCapitalize="none"
-                                autoCorrect={false}
-                                style={styles.input}
-                                maxLength={40}
-                            />
-                            <Text style={styles.label}>Digite o Username</Text>
-                            <TextInput
-                                onChangeText={val => config.user = val}
-                                placeholder="USERNAME"
-                                autoCapitalize="none"
-                                autoCorrect={false}
-                                style={styles.input}
-                                maxLength={40}
-                            />
-                            <Text style={styles.label}>Digite a senha</Text>
-                            <TextInput
-                                onChangeText={val => config.pass = val}
-                                placeholder="PASSWORD"
-                                autoCapitalize="none"
-                                autoCorrect={false}
-                                style={styles.input}
-                                maxLength={40}
-                            />
-                            <Text style={styles.label}>Digite o nome do banco de dados</Text>
-                            <TextInput
-                                onChangeText={val => config.db = val}
-                                placeholder="DATABASE"
-                                autoCapitalize="none"
-                                autoCorrect={false}
-                                style={styles.input}
-                                maxLength={40}
-                            />
-                        </View>
-                        <TouchableOpacity onPress={handleConfig} style={styles.submitBtn}>
-                            <Text style={styles.submitBtnText}>Salvar</Text>
-                        </TouchableOpacity>
+
+                <View style={styles.body}>
+                    <View style={styles.form}>
+                        <Text style={styles.label}>Nome de usuario</Text>
+                        <TextInput
+                            onChangeText={val => account.nm_usuario = val}
+                            placeholder="NOME"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            value={account.nm_usuario}
+                            style={styles.input}
+                            maxLength={40}
+                        />
+                        <Text style={styles.label}>E-mail </Text>
+                        <TextInput
+                            onChangeText={val => account.nm_email = val}
+                            placeholder="E-MAIL"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            value={account.nm_email}
+                            style={styles.input}
+                            maxLength={40}
+                        />
+                        <Text style={styles.label}>Senha</Text>
+                        <TextInput
+                            onChangeText={val => account.cd_senha = val}
+                            placeholder="SENHA"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            style={styles.input}
+                            value={account.cd_senha}
+                            secureTextEntry={true}
+                            maxLength={40}
+                        />
+                        <Text style={styles.label}>Digite novamente a senha</Text>
+                        <TextInput
+                            onChangeText={val => newPass = val}
+                            placeholder="SENHA"
+                            autoCapitalize="none"
+                            value={account.cd_senha}
+                            autoCorrect={false}
+                            style={styles.input}
+                            maxLength={40}
+                        />
                     </View>
-                {/* </SafeAreaView> */}
-            </KeyboardAvoidingView>
-        </ScrollView>
+                    <TouchableOpacity onPress={handleSubmit} style={[styles.submitBtn,styles.backBlue]}>
+                        <Text style={styles.submitBtnText}>Salvar</Text>
+                    </TouchableOpacity>
+                    {/* <TouchableOpacity onPress={handleSubmit} style={[styles.submitBtn,styles.backRed]}>
+                        <Text style={styles.submitBtnText}>Apagar conta</Text>
+                    </TouchableOpacity> */}
+                </View>
     );
 }
 const styles = StyleSheet.create({
@@ -112,12 +131,27 @@ const styles = StyleSheet.create({
     },
     submitBtn: {
         borderRadius: 10,
-        backgroundColor: '#4E7AF3',
+
         width: 310,
         margin: 10,
         alignSelf: 'center',
         bottom: 0,
-        // position: 'absolute'
+        position: 'absolute'
+    },
+    backBlue: {
+        backgroundColor: '#4E7AF3',
+    },
+    backRed: {
+        backgroundColor: '#4E7AF3',
+    },
+    submitBtn: {
+        borderRadius: 10,
+
+        width: 310,
+        margin: 10,
+        alignSelf: 'center',
+        bottom: 0,
+        position: 'absolute'
     },
     submitBtnText: {
         fontSize: 20,
