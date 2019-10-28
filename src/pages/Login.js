c881bd59cc12852949c5e8a590410d214884be5b
Victor Gazolli <victorgazolli@compmac.local>
2019-10-27 22:17:21 -0300

    login feature

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,116 +1,152 @@
 
-import React, {useEffect}from 'react';
-import { KeyboardAvoidingView, Platform ,StyleSheet, Image, TextInput, Text, ToastAndroid,TouchableOpacity} from 'react-native'
+import React, { useEffect } from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Image, TextInput, Text, ToastAndroid, TouchableOpacity } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
+import api from '../services/api';
+var account = {
+    name: '',
+    email: '',
+    password: '',
+    id: ''
+}
+export default function Login({ navigation }) {
+    useEffect(async () => {
+        try {
+            AsyncStorage.getItem('@account_id').then(stored_id => {
+                try {
+                    console.log(stored_id)
+                    if (stored_id.length > 0) {
+                        async function getAccount() {
+                            const response = await api.get('/user/' + stored_id)
+                            account = {
+                                name: response.data.response.name,
+                                email: response.data.response.email,
+                                password: response.data.response.password,
+                                id: response.data.response._id,
+                            }
+                        }
+                        getAccount()
+                        navigation.navigate('Produtos', account)
+                    }
+                } catch{
 
-var email = '';
-var password = '';
-export default function Login ({ navigation }) {
-    
-    useEffect( async () => {
-        try{
-            AsyncStorage.getItem('@account_email').then(user => {
-                if (user.length > 0) {
-                    navigation.navigate('Principal')
-                }else{
-                    
                 }
-              })
-        }catch(err){
-            
-        }
-        
-      }, []);
 
-  async function handleLogin () {
-        //await AsyncStorage.setItem('username', username);
-        //const response = await api.post('/users', { username: user })
-        navigation.navigate('Produtos')
-        // AsyncStorage.getItem('@account_email').then(user => {
-        //     if (user == email) {
-        //     }else{
-        //         ToastAndroid.show("Email não cadastrado", ToastAndroid.SHORT);
-                
-        //     }
-        //   })
+            })
+
+        } catch (err) {
+
+        }
+
+    }, []);
+    async function handleLogin() {
+        // await AsyncStorage.setItem('username', username);
+        // navigation.navigate('Produtos')
+        if (account.email.trim().length > 0 && account.password.trim().length > 0) {
+            async function getAccount() {
+                // var registered_account = {};
+                // console.log(account)
+                try {
+                    const { data } = await api.post('/user/login', { email: account.email, password: account.password })
+                    const response = JSON.parse(JSON.stringify(data))
+                    if (response.length > 0) {
+                        accessGranted(response[0])
+                        // console.log(response[0])
+                    } else {
+                        ToastAndroid.show("Login ou senha invalidos", ToastAndroid.SHORT)
+                    }
+                } catch (err) {
+                    console.log(err)
+
+                }
+
+
+                // navigation.navigate('Produtos')//, account)
+                // registered_account.id.length != null ? checkCredencials(registered_account.password) : ToastAndroid.show("Login inválido", ToastAndroid.SHORT);;
+            }
+            getAccount();
+        } else {
+            ToastAndroid.show("Digite corretamente os campos", ToastAndroid.SHORT)
+        }
+
     }
-    function handleRegister(){
+    function handleRegister() {
         navigation.navigate('CadastrarLogin');
     }
-    
-    
-  return (
-    <KeyboardAvoidingView
-        behavior="padding"
-        enabled={Platform.OS === 'ios'}
-        style={styles.container}>
-            <Image
-          style={{width: 150, height: 150, marginBottom: 25}}
-          source={require('../assets/icon.png')}
-        />
+
+    async function accessGranted(user) {
+        await AsyncStorage.setItem('@account_id', user.cd_usuario.toString());
+        navigation.navigate('Produtos', user);
+    }
+    return (
+        <KeyboardAvoidingView
+            behavior="padding"
+            enabled={Platform.OS === 'ios'}
+            style={styles.container}>
             <TextInput placeholder="Digite seu e-mail"
-            placeholderTextColor="#999"
-            autoCapitalize="none"
-            autoCorrect={false}
-            onChangeText={val => email = val}
-            style={[styles.input]}
+                placeholderTextColor="#999"
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={val => account.email = val}
+                style={[styles.input]}
             />
             <TextInput placeholder="Digite sua senha"
-            placeholderTextColor="#999"
-            autoCapitalize="none"
-            autoCorrect={false}
-            onChangeText={val => password = val}
-            secureTextEntry={true}
-            style={[styles.input]}
+                placeholderTextColor="#999"
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={val => account.password = val}
+                secureTextEntry={true}
+                style={[styles.input]}
             />
-            <TouchableOpacity style={[styles.button,styles.colorBackground]} onPress={handleLogin} >
+            <TouchableOpacity style={[styles.button, styles.colorBackground]} onPress={handleLogin} >
                 <Text style={styles.buttontext}>Fazer login</Text>
             </TouchableOpacity>
             <TouchableOpacity style={[styles.button, styles.colorBorder]} onPress={handleRegister} >
-                <Text style={[styles.buttontext,styles.textColor]}>Não tem uma conta? clique aqui</Text>
+                <Text style={[styles.buttontext, styles.textColor]}>Não tem uma conta? clique aqui</Text>
             </TouchableOpacity>
-    </KeyboardAvoidingView>
-  );
+        </KeyboardAvoidingView>
+    );
 };
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         justifyContent: "center",
         flex: 1,
         alignItems: "center",
         padding: 30,
         backgroundColor: '#fff'
     },
-    input:{
-        height:46,
+    input: {
+        height: 46,
         alignSelf: "stretch",
-        backgroundColor: "#f9f9f9",
+        backgroundColor: "#fcfcfc",
         borderRadius: 4,
         marginTop: 10,
+        color: "#000",
         borderColor: "#4E7AF3",
         borderBottomWidth: 1,
         paddingHorizontal: 15,
     },
-    button:{
-        height:46,
+    button: {
+        height: 46,
         alignSelf: "stretch",
         borderRadius: 4,
         marginTop: 10,
         justifyContent: 'center',
         alignItems: "center"
     },
-    colorBackground:{
+    colorBackground: {
         backgroundColor: '#4E7AF3',
     },
-    colorBorder:{
+    colorBorder: {
         borderWidth: 1,
         borderColor: "#4E7AF3",
         marginBottom: 100
     },
-    textColor:{
+    textColor: {
         color: "#4E7AF3",
     },
-    buttontext:{
+    buttontext: {
         color: "#FFF",
         fontWeight: 'bold',
         fontSize: 16
